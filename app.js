34a12a65f93b41ac0a0bf409c8b1833320983a82
Erik <erikjensenpratt+git@gmail.com>
2018-08-31 15:52:47 -0700

    Imported and required pg

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const pg = require("pg");
 const app = express();
 const port = 3000;
 
